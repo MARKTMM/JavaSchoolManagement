dd048986af305bec7e6665186aba9de9681f124f
MARKTMM <markmwangi304@gmail.com>
2024-03-26 18:37:40 +0300

    Spring Boot microservice

---
+++ src/main/java/com/itsmark/JavaSchoolManagement/JavaSchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.itsmark.JavaSchoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSchoolManagementApplication.class, args);
+	}
+
+}
